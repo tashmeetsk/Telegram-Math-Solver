bac4f2a95f528afccc4dadb8ad7938a95ab661e5
Mr-Uzumaki-Naruto <65593748+Mr-Uzumaki-Naruto@users.noreply.github.com>
2021-06-23 01:54:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ img = cv2.imread(r'C:\Users\dell\Pictures\Blob3.png')
 pytesseract.pytesseract.tesseract_cmd = (r'C:\Program Files\Tesseract-OCR\tesseract.exe')
 result = pytesseract.image_to_string(img)
 print(result)
-		
+print(int(result))
