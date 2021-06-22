14d38eedd317835a7b7ef839d8ebf120ae83d348
Mr-Uzumaki-Naruto <65593748+Mr-Uzumaki-Naruto@users.noreply.github.com>
2021-06-23 01:48:57 +0530

    Updating

--- main.py
+++ main.py
@@ -1,37 +1,7 @@
-# import the following libraries
-# will convert the image to text string
+import cv2
 import pytesseract	
-
-# adds image processing capabilities
-from PIL import Image	
-
-# converts the text to speech
-import pyttsx3		
-
-#translates into the mentioned language
-from googletrans import Translator	
-
-# opening an image from the source path
-img = Image.open('text1.png')	
-
-# describes image format in the output
-print(img)						
-# path where the tesseract module is installed
-pytesseract.pytesseract.tesseract_cmd ='C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
-# converts the image to result and saves it into result variable
+img = cv2.imread(r'C:\Users\dell\Pictures\Blob3.png')
+pytesseract.pytesseract.tesseract_cmd = (r'C:\Program Files\Tesseract-OCR\tesseract.exe')
 result = pytesseract.image_to_string(img)
-# write text in a text file and save it to source path
-with open('abc.txt',mode ='w') as file:	
-	
-				file.write(result)
-				print(result)
-				
-p = Translator()					
-# translates the text into german language
-k = p.translate(result,dest='german')	
-print(k)
-engine = pyttsx3.init()
-
-# an audio will be played which speaks the test if pyttsx3 recognizes it
-engine.say(k)							
-engine.runAndWait()
+print(result)
+		
