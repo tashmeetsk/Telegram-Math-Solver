10788938f3c4efce6a0f44db1cd2a302b28b2fa5
Misty <86200358+Misus-Misty@users.noreply.github.com>
2021-08-27 00:09:01 +0530

    Update main.py
    Just felt like changing some things which needed to be changed.................wahahahahahahahahahahaha

--- main.py
+++ main.py
@@ -21,24 +21,19 @@ def solver():
     result = pytesseract.image_to_string(blackAndWhiteImage2)
     print(result)
 
-    dict1 = {'§': '5', '%': 'x', '—': '-'} 
+    dict1 = {'§':'5', '%':'x', '—':'-'} 
         for i in dict1.keys():
             if i in result:
                 result = result.replace(i, dict1[i])
 
     numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    op = ['+', '-','x', '/','=']
+    op = ['+', '-', 'x', '/', '=']
 
     a,b,c,f = ([] for i in range(4))
 
     x = list(result)
 
-    for i in x:
-        if i in op or i in numbers:
-            if i in op and i in f:
-                continue
-            else:
-                f.append(i)
+    f = [i for i in x if i in op or i in numbers]  
 
     flag = 0
     for i in f:
@@ -66,10 +61,10 @@ def solver():
             isEqual = True
         return isEqual
 
-    lmao = calc(y,z,w)
-    print(lmao)
+    check = calc(y,z,w)
+    print(check)
 
-    if lmao == True:
+    if check == True:
         pyautogui.click(r'C:\Users\dell\AppData\Local\Programs\Python\Python39\Project\Correct.png')
     else:
         pyautogui.click(r'C:\Users\dell\AppData\Local\Programs\Python\Python39\Project\Incorrect.png')
