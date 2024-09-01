6069a823651b23b3c9a9806d96148d2e817e5916
Tashmeet <155848297+tashmeetsk@users.noreply.github.com>
2024-09-01 13:19:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 def solver(flag):
     x = int(driver.find_element(By.ID, "task_x").text)
     y = int(driver.find_element(By.ID, "task_y").text)
@@ -14,48 +13,37 @@ def solver(flag):
         operation = {"–": x.__sub__, "+": x.__add__, "×": x.__mul__, "/": x.__truediv__}
         final_ans = operation[op](y)
 
-    if flag == 0:
-        if game_ans == final_ans:
-            driver.find_element(By.ID, "button_correct").click()
-        else:
-            driver.find_element(By.ID, "button_wrong").click()
-
+    if (flag == 0 and game_ans == final_ans) or (flag == 1 and game_ans != final_ans):
+        driver.find_element(By.ID, "button_correct").click()
     else:
-        if game_ans == final_ans:
-            driver.find_element(By.ID, "button_wrong").click()
-        else:
-            driver.find_element(By.ID, "button_correct").click()
+        driver.find_element(By.ID, "button_wrong").click()
 
+ask = input('Do you have the link to the website? [y/n]: ').strip().lower()
 
-ask = input('Do you have the link of the website [y/n]: ')
-
-if ask.lower() == 'y':
-    website = input('''\nPlease enter the math solver site...
-Press Return key to continue by the default website \n''')
+if ask == 'y':
+    website = input('Please enter the math solver site URL (press the return key to continue with the default site)\n') or "https://tbot.xyz/math/"
 else:
     website = "https://tbot.xyz/math/"
 
-print()
-
 while True:
     try:
-        ask = int(input('Up to which number would you like to score (enter value in +ve integer): '))
+        target_score = int(input('Up to which number would you like to score? (Enter a positive integer): '))
+        if target_score > 0:
+            break
+        else:
+            print('Please enter a valid positive number.')
     except ValueError:
-        print('\nPlease enter a valid number...')
-    else:
-        break
-    if ask < 0:
-        print('\nPlease enter a valid number...')
+        print('Please enter a valid number.')
 
-option = webdriver.FirefoxOptions()
-driver = webdriver.Firefox(options = option)
+options = webdriver.FirefoxOptions()
+driver = webdriver.Firefox(options=options)
 
 driver.get(website)
 
-play = driver.find_element(By.ID, "button_correct").click()
+driver.find_element(By.ID, "button_correct").click()
 
-for i in range(0, int(ask)):
+for _ in range(target_score):
     solver(flag=0)
 
-for i in range(0, 3):
+for _ in range(3):
     solver(flag=1)
